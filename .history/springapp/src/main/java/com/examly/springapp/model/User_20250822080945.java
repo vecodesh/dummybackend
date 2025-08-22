f706a5ac13850671ade5eeebfe3dcc8f12a000a9
vecodesh <vecodesh@gmail.com>
2025-08-23 00:17:07 +0530

    D

--- .history/springapp/src/main/java/com/examly/springapp/model/User_20250822080945.java
+++ .history/springapp/src/main/java/com/examly/springapp/model/User_20250822080945.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import java.util.*;
 
@@ -15,7 +14,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
